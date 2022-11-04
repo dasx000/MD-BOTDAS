48062d7fc9ea614027e1e7de57e36ac371b034af
FARUQOFC <113996402+Faruq51@users.noreply.github.com>
2022-11-04 14:04:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 global.hituet = 0
-global.gopayno = "0858-0726-4974"
-global.danano = "0858-0726-4974"
-global.shopeepayno = "0858-0726-4974"
-global.creator = "6285807264974@s.whatsapp.net"
+global.gopayno = "08Xxx"
+global.danano = "08Xxx"
+global.shopeepayno = "08Xxx"
+global.creator = "62Xxx@s.whatsapp.net"
 global.thumb = fs.readFileSync(`./image/thumb.png`)
 global.qrisdonate = fs.readFileSync(`./image/qris.jpg`)
 global.fake = `© Kanna Maker`
@@ -15,9 +15,9 @@ global.antilink = false
 global.antiwame = false
 global.autodltt = false
 global.autosticker = false
-global.ownerNomor = '6285807264974'
+global.ownerNomor = '62Xxxxxx'
 global.ownerName = 'Puncak comedi'
-global.ownerNumber = ["6285807264974@s.whatsapp.net"]
+global.ownerNumber = ["62Xxxxx@s.whatsapp.net"]
 global.cek1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 global.mess = {
     wait: 'Wait Sis Please be patient',
