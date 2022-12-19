bdc3cd129a5c320aba43824e6d1c3a39e0278e8e
dasx000 <dikysaputra2025@gmail.com>
2022-12-19 21:52:15 +0700

    lah

--- kayla.js
+++ kayla.js
@@ -1436,6 +1436,62 @@ Title : ${atdl.title}`,
 
     switch (command) {
       case 'convert':
+        /////////////// function  CONVERTER _=_=_=_=
+        // FUNCTION CONVERTER DOCUMENT
+        const convert = async (s) => {
+          // miemtype
+          if (args[0] === 'pdf') {
+            mimeType = 'application/pdf';
+          } else if (args[0] === 'doc') {
+            mimeType = 'application/msword';
+          } else if (args[0] === 'zip') {
+            mimeType = 'application/zip';
+          } else if (args[0] === 'jpg') {
+            mimeType = 'image/jpg';
+          } else if (args[0] === 'png') {
+            mimeType = 'image/png';
+          } else if (args[0] === 'jpeg') {
+            mimeType = 'image/jpeg';
+          } else if (args[0] === 'docx') {
+            mimeType =
+              'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+          } else if (args[0] === 'xlsx') {
+            mimeType =
+              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+          } else if (args[0] === 'xls') {
+            mimeType = 'application/vnd.ms-excel';
+          }
+
+          convertapi
+            .convert(args[0], {
+              File: media,
+            })
+            .then(function (result) {
+              result.saveFiles(`./media/result.${args[0]}`);
+            })
+            .catch((err) => {
+              return reply(err.message);
+            });
+
+          await sleep(s);
+          kayla.sendMessage(
+            m.chat,
+            {
+              document: {
+                url: `./media/result.${args[0]}`,
+              },
+              mimetype: mimeType,
+              fileName: `${fileName.split('.')[0]}.${args[0]}`,
+            },
+            { quoted: m }
+          );
+          await sleep(s);
+          await fs.unlinkSync(`./media/result.${args[0]}`);
+          console.log('succcess');
+        };
+
+        /////////////////
+
         // reply(args.length);
         if (args.length == 0) return reply(`convert to what?`);
         if (isQuotedDocument) {
