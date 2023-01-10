ca9c7116d62c85a67d32f1a377662b9f7ca128ae
DIKY ADISAPUTRA <68739579+dikyadisaputra@users.noreply.github.com>
2023-01-10 21:07:09 +0700

    ubah apikey

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const fs = require('fs');
-global.diky_key = 'diky';
+global.diky_key = ''; // bikin apikey disini https://api.diky.my.id/
 global.grupWa = 'https://chat.whatsapp.com/CfF9ehZcKrMJl8EXpYd11Q';
 global.linkRestApi = 'https://api.diky.my.id/';
 global.openaiKey = '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';
